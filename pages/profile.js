7ed1a9191d94fd4ffd2523ae22eff1ee9b480657
bashandd <bashandd@gmail.com>
2023-01-09 06:27:08 +0530

    Updated Profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,14 +1,18 @@
-import { Layout, Card } from "antd";
+import { Layout, Col, Card, Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 import MainLayout from "../components/layout/MainLayout";
-const { Content, Sider } = Layout;
 
+const { Content, Sider } = Layout;
 
 function Profile() {
   // console.log(" I am in profile page");
   let userInfo = "";
 
   function getUser() {
-    userInfo = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem("auth")): null;
+    userInfo =
+      typeof window !== "undefined"
+        ? JSON.parse(localStorage.getItem("auth"))
+        : null;
 
     if (userInfo) {
       return userInfo.user;
@@ -18,27 +22,47 @@ function Profile() {
   }
 
   userInfo = getUser();
-if (userInfo){
-  return (
-    <MainLayout>
-     
+  if (userInfo) {
+    return (
+      <MainLayout>
+        {/* <Col span={12}> */}
+        <Avatar
+          size={{
+            xs: 24,
+            sm: 32,
+            md: 40,
+            lg: 64,
+            xl: 80,
+            xxl: 100,
+          }}
+          icon={<UserOutlined />}
+          style={{
+            color: "#eb2f96",
+            backgroundColor: "#fde3cf",
+            marginLeft: "550px",
+            marginTop: "100px",
+          }}
+        />
         <Card
-          title="Profile"
-           bordered={true}
-          style={{ align: "center", width: 300 }}
+          title={userInfo.name}
+          bordered={true}
+          style={{
+            width: 400,
+            marginLeft: "400px",
+
+            borderRadius: "20px",
+            overflow: "hidden",
+          }}
         >
-          <h1>Name: {userInfo.name}</h1>
           <h1>Email: {userInfo.email}</h1>
           <h1>Phone: {userInfo.phone}</h1>
           <p>Role: {userInfo.role}</p>
           <p>Reports To: {userInfo.reportsTo}</p>
-         
         </Card>
-
-    </MainLayout>
-  );
-}
-  
+        {/* </Col> */}
+      </MainLayout>
+    );
+  }
 }
 
 export default Profile;
